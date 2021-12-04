07e19c3b01b55dfca3f68e85f4969c3423093c86
Thomas Lee <thomas.lee@allianzdirect.de>
2021-12-04 16:46:55 +0100

    fix: put kafka back to main thread

--- src/test/kotlin/com/mms/oms/support/InMemoryKafka.kt
+++ src/test/kotlin/com/mms/oms/support/InMemoryKafka.kt
@@ -2,16 +2,12 @@ package com.mms.oms.support
 
 import com.consol.citrus.kafka.embedded.EmbeddedKafkaServer
 import com.consol.citrus.kafka.embedded.EmbeddedKafkaServerBuilder
-import kotlin.concurrent.thread
 
 class InMemoryKafka {
     private val embeddedKafka: EmbeddedKafkaServer = EmbeddedKafkaServerBuilder().kafkaServerPort(59092).build()
 
     fun start() {
-        thread(isDaemon = true, name = "in-memory-kafka") {
-            embeddedKafka.start()
-        }
-        Thread.sleep(5000)
+        embeddedKafka.start()
     }
 
     fun stop() {
